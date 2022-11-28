812d2711f87f5b7e613be5db2e6a5a3f91a5236b
Akhila <akhilak2505@gmail.com>
2022-11-29 01:52:13 +0530

    Automated step 4-5 and readme.md updated

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 1. Install Android Studio 2021.1.1
 2. JDK 1.8 or greater
 3. Appium Server v1.22.3
+4. IntelliJ IDEA IDE
 
 ## Configure steps ##
 1. Open Android studio -> Device Manager and create Device
 2. Install the TheScore APK in emulator using adb install <theScore.apk> command from command prompt.
-3. Clone the git repo
+3. Clone the git repo and open in IntelliJ IDEA IDE
 4. Update the config.properties file.
+5. Start the Appium Server
+6. Start the Emulator from Android studio.
 5. Run the test using mvn clean test command.
--- src/test/java/TC01_TheScore.java
+++ src/test/java/TC01_TheScore.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -17,23 +18,27 @@ public class TC01_TheScore extends Base {
     By onlyThisTime=By.id("com.android.permissioncontroller:id/permission_allow_one_time_button");
     //By done= By.id("com.fivemobile.thescore:id/action_button_text");
 
-    By done=By.id("com.fivemobile.thescore:id/action_button_text");
+    By done=By.xpath("//android.view.ViewGroup[@resource-id='com.fivemobile.thescore:id/btn_primary']");
 
-    By nFLLeauges = By.xpath("//android.widget.FrameLayout[@content-desc=\"Leagues\"]/android.widget.FrameLayout/android.widget.ImageView");
-
-    By myLeagues=By.id("com.fivemobile.thescore:id/navigation_bar_item_small_label_view");
-
-    //leagues title
-    By leaguesTitle=By.id("com.fivemobile.thescore:id/titleTextView");
+    //Leagues in bottom right
+    By leaguesIcon = By.xpath("//android.widget.FrameLayout[@content-desc=\"Leagues\"]/android.widget.FrameLayout/android.widget.ImageView");
 
     //Leagues page NFL
     By leaguesNFL= By.id("com.fivemobile.thescore:id/league_name_text");
 
-    //standings locator
-    By standings=By.xpath("//android.widget.LinearLayout[@content-desc=\"Standings\"]/android.widget.TextView");
+    //leagues title
+    By leaguesTitle=By.id("com.fivemobile.thescore:id/titleTextView");
 
+    //standings locator
+    By standings=By.xpath("//android.widget.LinearLayout[@content-desc='Standings']/android.widget.TextView");
+
+    //back arrow
+    By backArrow=By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']");
+
+    //AFC tab
+    By aFCTab=By.xpath("//android.widget.LinearLayout[@content-desc=\"AFC\"]/android.widget.TextView");
     @Test
-    public void basicTest() throws InterruptedException {
+    public void basicTest() {
 
 
         //click on get started
@@ -52,31 +57,30 @@ public class TC01_TheScore extends Base {
         wait.until(ExpectedConditions.visibilityOfElementLocated(onlyThisTime)).click();
 
         //click on Done
-        wait.until(ExpectedConditions.elementToBeClickable(done)).click();
-
-        Thread.sleep(5000);
+        wait.until(ExpectedConditions.presenceOfElementLocated(done)).click();
 
         //Click on NFL
-        wait.until(ExpectedConditions.visibilityOfElementLocated(nFLLeauges)).click();
-
-        //select My Leagues
-        wait.until(ExpectedConditions.visibilityOfElementLocated(myLeagues)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(leaguesIcon)).click();
 
         //Verify that the expected page opens correctly.
         Assert.assertTrue(driver.findElement(leaguesTitle).isDisplayed());
 
-        //Tap on a sub-tab of your choice, eg: league table / standings / leaders, or stats tab of the
-        //league, team, or player
-        //click on NFL
+        //select My Leagues
         wait.until(ExpectedConditions.visibilityOfElementLocated(leaguesNFL)).click();
 
-        //click on standings
+        //Tap on a sub-tab of your choice, eg: league table / standings / leaders, or stats tab of the
+        //league, team, or player
         wait.until(ExpectedConditions.visibilityOfElementLocated(standings)).click();
 
         //Verify that you are on the correct tab and that the data is displayed correctly and
         //corresponds to the league, team, or player from step 1.
+        Assert.assertTrue(driver.findElement(aFCTab).isDisplayed());
 
+        //navigate back
+        wait.until(ExpectedConditions.visibilityOfElementLocated(backArrow)).click();
 
+        //Verify back os working fine
+        Assert.assertTrue(driver.findElement(leaguesTitle).isDisplayed());
     }
 
 }
